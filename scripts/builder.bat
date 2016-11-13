3aaf839b7abeb4ed9048b5f8c1981b4564bd8698
tamtadam <trenyik.adam@gmail.com>
2016-11-13 16:56:50 +0100

    fix

--- scripts/builder.bat
+++ scripts/builder.bat
@@ -1,11 +1,6 @@
-SET foo=%PATH%
-ECHO %foo%
-
 call perl %GIT_ROOT%\common\scripts\remove_trailing_lines.pl %*
 call %GIT_ROOT%\common\scripts\dos2unix.exe %*
 call perl %GIT_ROOT%\common\scripts\copy_from_git2xampp.pl %*
 
 call perl -wc %*
-
-ECHO %GIT_ROOT%
 
